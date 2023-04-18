ae86e4f8ea0a8ceb6658585f19126096f5501c16
Aman9868 <9868638501aman@gmail.com>
2023-04-18 11:54:10 +0530

    Uknown Commit

--- forms.py
+++ forms.py
@@ -1,9 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField,FileField,BooleanField
-from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError,InputRequired
+from wtforms.validators import Length, EqualTo, Email, DataRequired
 class RegisterForm(FlaskForm):
     username = StringField(label='User Name:', validators=[Length(min=2, max=30), DataRequired()])
     email = StringField(label='Email Address:', validators=[Email(), DataRequired()])
     password = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
     confirm_password = PasswordField(label='Confirm Password:', validators=[EqualTo('password'), DataRequired()])
     mobile_number = StringField('Mobile Number', validators=[DataRequired()])
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    password = PasswordField(label='Password:', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
