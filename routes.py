edbf6cbdb42350d065551e8f12bcc3631808863b
Aman9868 <9868638501aman@gmail.com>
2023-04-18 12:35:10 +0530

    Gpu memory cache clear commit

--- routes.py
+++ routes.py
@@ -1,5 +1,18 @@
 from app import app
 from flask import render_template,request,redirect,url_for, flash,jsonify
+import torch
+from numba import cuda
+from GPUtil import showUtilization as gpu_usage
+def free_gpu_cache():
+    print("Initial GPU Usage")
+    gpu_usage()
+    torch.cuda.empty_cache()
+    cuda.select_device(0)
+    cuda.close()
+    cuda.select_device(0)
+    print("GPU Usage after emptying the cache")
+    gpu_usage()
+free_gpu_cache()
 @app.route("/")
 @app.route("/home")
 def home_page():
