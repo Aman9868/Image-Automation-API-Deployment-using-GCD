ac0acb0525c398462a467a4abf8db6811b9e73cf
Aman9868 <9868638501aman@gmail.com>
2023-04-19 21:07:19 +0530

    Added Airport codes extractor functions

--- functions.py
+++ functions.py
@@ -153,3 +153,97 @@ def first_to_third_person(sentence):
     new_sentence = ' '.join(new_words)
     
     return new_sentence
+
+################----------------Airport Code Extraction----------------------------------##############
+def extract_airport_codes(sentence):
+    airport_codes = {
+        "JFK": "New York",
+        "LAX": "Los Angeles",
+        "ORD": "Chicago",
+        "DFW": "Dallas",
+        "DEN": "Denver",'IXA':'Agartala',
+'AGR':'Agra','AMD':'Ahmedabad','AJL':'Aizawl','ATQ':'Amritsar','IXU':'Aurangabad','IXB':'Bagdogra','BEK':'Bareilly','IXG':'Belagavi','BLR':'Bengaluru',
+'BHO':'Bhopal','BBI':'Bhubaneswar','IXC':'Chandigarh','MAA':'Chennai','CJB':'Coimbatore','DBR':'Darbhanga','DED':'Dehradun','DEL':'Delhi','DGH':'Deoghar',
+'DIB':'Dibrugarh','DMU':'Dimapur','RDP':'Durgapur','GAY':'Gaya','GOI':'Goa','GOP':'Gorakhpur','GAU':'Guwahati','GWL':'Gwalior',
+'HBX':'Hubli','HYD':'Hyderabad','IMF':'Imphal','IDR':'Indore','HGI':'Itanagar','JLR':'Jabalpur','JAI':'Jaipur','IXJ':'Jammu','JDH':'Jodhpur','JRH':'Jorhat',
+'CDP':'Kadapa','CNN':'Kannur','KNU':'Kanpur','COK':'Kochi','KLH':'Kolhapur','CCU':'Kolkata','CCJ':'Kozhikode','KJB':'Kurnool','IXL':'Leh','LKO':'Lucknow','IXM':'Madurai',
+'IXE':'Mangaluru','BOM':'Mumbai','MYQ':'Mysuru','NAG':'Nagpur','GOX':'North Goa','PGH':'Pantnagar','PAT':'Patna','IXZ':'Port-Blair','IXD':'Prayagraj','PNQ':'Pune',
+'RPR':'Raipur','RJA':'Rajahmundry','IXR':'Ranchi','SHL':'Shillong','SAG':'Shirdi','IXS':'Silchar','SXR':'Srinagar','STV':'Surat','TRV':'Thiruvrappalli','TIR':'Tirupati',
+'TCR':'Tuticorin','UDR':'Udaipur','BDQ':'Vadodara',
+'TRZ':'Tiruchirappalli','VNS':'Varanasi','VGA':'Vijayawada','VTZ':'Visakhapatnam','CNN':'Kannur','IXA':'Agartala',
+'AMD':'Ahmedabad','ATQ':'Amritsar','IXB':'Bagdogra',
+'BLR':'Bengaluru','BBI':'Bhubaneswar','BHO':'Bhopal','IXC':'Chandigarh','MAA':'Chennai','CJB':'Coimbatore','DED':'Dehradun','DEL':'Delhi','DMU':'Dimapur',
+'GOI':'Goa','GOP':'Gorakhpur','GAU':'Guwahati','HBX':'Hubli','HYD':'Hyderabad','IMF':'Imphal','IDR':'Indore','JLR':'Jabalpur',
+'JAI':'Jaipur','IXJ':'Jammu','CCU':'Kolkata','CCJ':'Kozhikode','LKO':'Lucknow','IXM':'Madurai','IXE':'Mangaluru',
+'BOM':'Mumbai','NAG':'Nagpur','PAT':'Patna','PNQ':'Pune','RPR':'Raipur','RJA':'Rajahmundry','IXR':'Ranchi',
+'SXR':'Srinagar','STV':'Surat','TRV':'Thiruvananthapuram','TRZ':'Tiruchirappalli','TCR':'Tuticorin','UDR':'Udaipur',
+'BDQ':'Vadodara','VNS':'Varanasi','VGA':'Vijayawada','VTZ':'Visakhapatnam','GAY':'Gaya','JDH':'Jodhpur','IXS':'Silchar','IXG':'Belagavi','YVR': 'Vancouver',
+'YYC': 'Calgary',
+    'YEG': 'Edmonton',
+    'YWG': 'Winnipeg',
+    'YYZ': 'Toronto',
+    'YOW': 'Ottawa',
+    'YUL': 'Montreal',
+    'YQB': 'Quebec City',
+    'YHZ': 'Halifax',
+    'YYT': 'St. John',
+	"ATL": "Atlanta",
+    "BOS": "Boston",
+    "CLT": "Charlotte",
+    "ORD": "Chicago",
+    "CVG": "Cincinnati",
+    "DFW": "Dallas/Fort Worth",
+    "DEN": "Denver",
+    "DTW": "Detroit",
+    "HNL": "Honolulu",
+    "IAH": "Houston",
+    "LAS": "Las Vegas",
+    "LAX": "Los Angeles",
+    "MEM": "Memphis",
+    "MIA": "Miami",
+    "MSP": "Minneapolis",
+    "JFK": "New York City",
+    "EWR": "Newark",
+    "LGA": "New York City",
+    "OAK": "Oakland",
+    "MCO": "Orlando",
+    "PHL": "Philadelphia",
+    "PHX": "Phoenix",
+    "PDX": "Portland",
+    "SLC": "Salt Lake City",
+    "SAN": "San Diego",
+    "SFO": "San Francisco",
+    "SJC": "San Jose",
+    "SEA": "Seattle",
+    "STL": "St. Louis",
+    "TPA": "Tampa",
+    "DCA": "Washington, D.C.",
+	"LHR": "London Heathrow Airport",
+    "LGW": "London Gatwick Airport",
+    "STN": "London Stansted Airport",
+    "LTN": "London Luton Airport",
+    "MAN": "Manchester Airport",
+    "BHX": "Birmingham Airport",
+    "GLA": "Glasgow Airport",
+    "EDI": "Edinburgh Airport",
+    "NCL": "Newcastle Airport",
+    "BRS": "Bristol Airport",
+    "LPL": "Liverpool John Lennon Airport",
+    "EMA": "East Midlands Airport","ABZ": "Aberdeen Airport","BOH": "Bournemouth Airport",
+    "DSA": "Doncaster Sheffield Airport","EXT": "Exeter Airport","JER": "Jersey Airport",
+    "LBA": "Leeds Bradford Airport","LCY": "London City Airport","MME": "Durham Tees Valley Airport","NWI": "Norwich Airport","SOU": "Southampton Airport",
+    "PIK": "Glasgow Prestwick Airport","SEN": "London Southend Airport","SYY": "Stornoway Airport","SWS": "Swansea Airport"
+    }
+
+    # Regular expression pattern to match city names in the input sentence
+    city_pattern = r"\b(" + "|".join(airport_codes.values()) + r")\b"
+
+    # Extract source and destination city names from the input sentence
+    matches = re.findall(city_pattern, sentence)
+    source_city = matches[0]
+    destination_city = matches[1]
+
+    # Get the corresponding airport codes for the source and destination cities
+    source_code = [code for code, city in airport_codes.items() if city == source_city][0]
+    destination_code = [code for code, city in airport_codes.items() if city == destination_city][0]
+    return f"Source city is {source_code}"+ " \n " + f"Destination city is {destination_code}"
